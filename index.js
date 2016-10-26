5706c6c9d86130c639836fff179c6509f9ee3d8a
William Blythe <wbwilliam7@gmail.com>
2016-10-26 12:24:52 +1100

    things

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 'use strict';
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('I am ready!');
+	console.log('I am ready!');
 });
 
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
-  }
+	if (message.content === 'ping') {
+		message.reply('pong');
+	}
 });
 
 client.on('message', message => {
-  if (message.content === 'ding') {
-    message.reply('dong');
-  }
+	if (message.content === 'ding') {
+		message.reply('dong');
+	}
 });
-
 
 client.on('message', message => {
 	if (message.content === 'm8') {
-		message.reply('u wot')
+		message.reply('u wot');
 	}
-})
+});
 
 client.on('message', message => {
 	if (message.content === 'm8') {
-		message.reply('u wot')
+		message.reply('u wot');
 	}
-})
+});
 
 client.on('message', message => {
 	if (message.author.id !== '240632939375755265') {
-		message.reply(message.timestamp)
-}})
+		message.reply(message.timestamp);
+	}});
 
 client.login(process.env.DISCORD_TOKEN);
